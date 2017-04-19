45206afcd38ea97689b63383442e73f86dd3ce03
Yvan <yvmarques@users.noreply.github.com>
2017-04-19 16:22:06 +0200

    Update index.js

--- settings/index.js
+++ settings/index.js
@@ -21,6 +21,8 @@ var path = require("path");
 module.exports = {
   workingDirectory: path.join(__dirname, "..", "tmp"),
   storagePath: path.join(__dirname, "..", "media"),
+  redisHost: "127.0.0.1",
+  worker: true,
   fonts: [
     { family: "Ratio", file: path.join(__dirname, "fonts", "ratio_reg.ttf") },
     { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-Regular.ttf") },
